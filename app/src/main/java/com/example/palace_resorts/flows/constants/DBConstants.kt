266dd86df5cf0764bbc7f8726061c15e3c3b1f18
Alejandro Valencia <ariel.a.sanchez.v@gmail.com>
2023-12-18 23:31:20 -0600

    remove unused resources

--- app/src/main/java/com/example/palace_resorts/flows/constants/DBConstants.kt
+++ app/src/main/java/com/example/palace_resorts/flows/constants/DBConstants.kt
@@ -1,11 +1,6 @@
 package com.example.palace_resorts.flows.constants
 
 object DBConstants {
-    const val DATABASE_VERSION_CODE = 1
     const val DATABASE_NAME = "news.db"
     const val NEWS_TABLE = "news"
-    const val ARTICLE_TABLE = "articles"
-    const val NEWS_FAVORITES = "news_favorites"
-    const val NEWS_DETAILS_TABLE = "news_details"
-    const val ID = "id"
 }
--- app/src/main/java/com/example/palace_resorts/flows/models/NewsDatabase.kt
+++ app/src/main/java/com/example/palace_resorts/flows/models/NewsDatabase.kt
@@ -2,14 +2,11 @@ package com.example.palace_resorts.flows.models
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.example.palace_resorts.base.converters.NewsTypeConverters
 import com.example.palace_resorts.flows.dao.NewsDao
 
 @Database(
-    entities = [EntityNewsItem::class], version = 2, exportSchema = false
+    entities = [EntityNewsItem::class], version = 1, exportSchema = false
 )
-@TypeConverters(NewsTypeConverters::class)
 abstract class NewsDatabase : RoomDatabase() {
     abstract fun newsDao(): NewsDao
 }
